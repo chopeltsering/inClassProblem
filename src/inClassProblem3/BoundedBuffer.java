6b1b2ec035f3787ac20fe61a325227ae59615909
chopel tsering <chopeltsering@gmail.com>
2012-10-10 03:03:12 -0400

    yooo

---
+++ src/inClassProblem3/BoundedBuffer.java
@@ -0,0 +1,41 @@
+
+package inClassProblem3;
+public class BoundedBuffer
+{  
+    public static final int SIZE = 5;
+    private Object[] buffer = new Object[SIZE];
+    private int inIndex = 0, outIndex = 0, count = 0;
+
+
+    public boolean addLast(Object item)
+    {
+	    if (count == SIZE) return false; // full
+	    buffer[inIndex] = item;
+	    inIndex = (inIndex + 1) % SIZE;
+//	    count++;
+//	    if (count == SIZE)
+//	    	writeable = false; // now full
+//	    else if (count == 1) // was empty
+//	    	readable = true;
+	    return true;
+    }
+
+    public Object removeFirst()
+    {
+	    Object item;
+	    if (count == 0) return null; // empty
+	    item = buffer[outIndex];
+	    outIndex = (outIndex + 1) % SIZE;
+//	    count--;
+//	    if (count == 0)
+//	    	readable = false; // now empty
+//	    else if (count == SIZE-1) // was full
+//	    	writeable = true;
+	    return item;
+    }
+    public int getCount()
+    {
+    	return count;
+    }
+    
+}
