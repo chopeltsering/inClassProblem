1d8f9ebcf360d28d95d002362613e15aff6f8618
unknown <tchopel@WIN7-4155C-435.windows.scs.carleton.ca>
2012-10-11 15:48:49 -0400

    Signed-off-by: unknown <tchopel@HP3341-0221.windows.scs.carleton.ca>

---
+++ src/inClassProblem3/ChannelArray.java
@@ -0,0 +1,17 @@
+package inClassProblem3;
+
+public class ChannelArray implements Channel{
+
+	@Override
+	public void put(Object o) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object take() throws InterruptedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/inClassProblem3/TestChannel.java
+++ src/inClassProblem3/TestChannel.java
@@ -2,8 +2,8 @@ package inClassProblem3;
 
 public class TestChannel {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args){
+		
 
 	}
 
