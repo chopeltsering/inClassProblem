b3579084850339d8c3265b5de8bdcbeea30493d1
unknown <tchopel@WIN7-4155A-394.windows.scs.carleton.ca>
2012-10-16 16:01:53 -0400

    a test class to test stuff

---
+++ src/inClassProblem4_part1_byChopel/I_wanna_check_multiple_source_folder.java
@@ -0,0 +1,28 @@
+package inClassProblem4_part1_byChopel;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class I_wanna_check_multiple_source_folder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if(args.length<1){
+			System.out.println("usage: java dpl.sockets.NsLookup <hostName>");
+			System.exit(1);
+		}
+		InetAddress ia;
+		try{
+			ia = InetAddress.getByName(args[0]);
+			System.out.println("Name: " + args[0]);
+			System.out.println("Address: " + ia.getHostAddress());
+			System.out.println("hostName: " + ia.getHostName());
+		}catch(UnknownHostException e){
+			System.exit(2);
+		}
+
+	}
+
+}
